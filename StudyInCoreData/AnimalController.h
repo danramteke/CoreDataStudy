177ea92a8f02908cedbe2d1722b025b1936eae38
Cyrus Team <cyrusteam@cyruslists.com>
2013-12-19 16:48:02 -0500

    strings file

---
+++ StudyInCoreData/AnimalController.h
@@ -0,0 +1,13 @@
+//
+//  AnimalController.h
+//  StudyInCoreData
+//
+//  Created by Cyrus on 12/19/13.
+//  Copyright (c) 2013 Daniel Ramteke. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AnimalController : NSObject
+-(instancetype)initWithContext:(NSManagedObjectContext*)context;
+@end
---
+++ StudyInCoreData/AnimalController.m
@@ -0,0 +1,19 @@
+//
+//  AnimalController.m
+//  StudyInCoreData
+//
+//  Created by Cyrus on 12/19/13.
+//  Copyright (c) 2013 Daniel Ramteke. All rights reserved.
+//
+
+#import "AnimalController.h"
+
+@implementation AnimalController
+-(instancetype)initWithContext:(NSManagedObjectContext*)context {
+    self = [super init];
+    if (self) {
+        
+    }
+    return self;
+}
+@end
